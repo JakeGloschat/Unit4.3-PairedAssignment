6107a51a20a1417e671344a6e017c0a673ec4568
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-22 13:25:58 -0700

    Just did a littlefile organization in preperation for the project.

---
+++ BYOA4.3/BYOA4.3/ModelController/ModelController1.swift
@@ -0,0 +1,8 @@
+//
+//  ModelController1.swift
+//  BYOA4.3
+//
+//  Created by Jake Gloschat on 2/22/23.
+//
+
+import Foundation
---
+++ BYOA4.3/BYOA4.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BYOA4.3
+//
+//  Created by Jake Gloschat on 2/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
